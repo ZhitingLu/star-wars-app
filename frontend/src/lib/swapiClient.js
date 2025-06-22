adb141e35309592968d3a32607f734d560f05a08
Zhiting Lu <zhitinglu@outlook.com>
2025-06-22 17:49:58 +0200

    add swapiClient utility for fetching paginated people and planets lists

---
+++ frontend/src/lib/swapiClient.js
@@ -0,0 +1,38 @@
+// lib/swapiClient.js
+
+import { fetcher } from "./fetcher";
+
+const BASE_API_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000/api";
+
+/**
+ * Fetch paginated, searchable, sortable people list
+ * @param {object} params - page, search, sort_by, order
+ */
+export async function fetchPeople(params = {}) {
+  const url = new URL(`${BASE_API_URL}/people`);
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      url.searchParams.append(key, value);
+    }
+  });
+
+  return fetcher(url.toString());
+}
+
+/**
+ * Fetch paginated, searchable, sortable planets list
+ * @param {object} params - page, search, sort_by, order
+ */
+export async function fetchPlanets(params = {}) {
+  const url = new URL(`${BASE_API_URL}/planets`);
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined && value !== null) {
+      url.searchParams.append(key, value);
+    }
+  });
+
+  return fetcher(url.toString());
+}
