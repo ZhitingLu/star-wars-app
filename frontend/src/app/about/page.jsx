7c603b75b8a9fd2609816d91bd0cb0af00a5ddea
Zhiting Lu <zhitinglu@outlook.com>
2025-06-22 17:49:37 +0200

    add About and People components

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <main className="overflow-hidden">About page</main>;
+}
---
+++ frontend/src/app/people/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function People() {
+  return <main className="overflow-hidden">people page</main>;
+}
