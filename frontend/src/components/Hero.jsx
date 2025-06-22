76984e0af20b1d31aae05836ba537e107cbb44b4
Zhiting Lu <zhitinglu@outlook.com>
2025-06-22 19:47:46 +0200

    add React import to Hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
