cb928d3bd491062250a74b56e2632b5356dafef6
Zhiting Lu <zhitinglu@outlook.com>
2025-06-23 09:45:09 +0200

    feat: update Home component to include PeopleTable and PlanetsTable

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,22 @@
-import HeroPage from "@/components/Hero";
+import HeroPage from "@/components/HeroPage";
+import PeopleTable from "@/components/PeopleTable";
+import PlanetsTable from "@/components/PlanetsTable";
 
 export default function Home() {
   return (
-   <div>
+   <>
     <HeroPage />
-   </div>
+    <main className="bg-black text-white px-6 py-12">
+        <section id="people" className="my-20">
+          <h2 className="text-3xl font-bold mb-6">People</h2>
+          <PeopleTable />
+        </section>
+
+        <section id="planets" className="my-20">
+          <h2 className="text-3xl font-bold mb-6">Planets</h2>
+          <PlanetsTable />
+        </section>
+      </main>
+   </>
   );
 }
