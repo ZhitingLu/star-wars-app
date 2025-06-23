5e9fd118b3378755ede3ac4ed64c0d38f9cea5ef
Zhiting Lu <zhitinglu@outlook.com>
2025-06-23 20:07:58 +0200

    fix: wrap Home component in a div for proper layout and background video display

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,9 +4,20 @@ import PlanetsTable from "@/components/PlanetsTable";
 
 export default function Home() {
   return (
-   <>
+   <div className="min-h-screen relative text-white">
+    {/* Background video behind everything */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="fixed inset-0 z-[-1] w-full h-full object-cover"
+      >
+        <source src="/stars.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     <HeroPage />
-    <main className="bg-black text-white px-6 py-12">
+    <main className="bg-black/70 text-white px-6 py-12">
         <section id="people" className="my-20">
           <h2 className="text-3xl font-bold mb-6">People</h2>
           <PeopleTable />
@@ -17,6 +28,6 @@ export default function Home() {
           <PlanetsTable />
         </section>
       </main>
-   </>
+   </div>
   );
 }
