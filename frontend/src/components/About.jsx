84d9b0703a8ad647e0f633773045b0b903029b83
Zhiting Lu <zhitinglu@outlook.com>
2025-06-24 11:46:02 +0200

    add hover effects to images in About component

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,12 +10,14 @@ export default function About() {
               About the Explorer
             </h2>
             <p className="mt-6 text-xl/8 text-gray-300">
-              Star Wars Explorer lets you dive into the iconic galaxy far, far away. 
-              Discover detailed profiles of characters and learn about the fascinating planets they inhabit — all powered by the SWAPI.
+              Star Wars Explorer lets you dive into the iconic galaxy far, far
+              away. Discover detailed profiles of characters and learn about the
+              fascinating planets they inhabit — all powered by the SWAPI.
             </p>
             <p className="mt-6 text-base/7 text-gray-300">
-              Whether you're a longtime fan or just curious about the universe, 
-              this app gives you quick data on the people, places, and lore of Star Wars — right at your fingertips.
+              Whether you're a longtime fan or just curious about the universe,
+              this app gives you quick data on the people, places, and lore of
+              Star Wars — right at your fingertips.
             </p>
           </div>
           <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
@@ -23,7 +25,8 @@ export default function About() {
               <img
                 alt=""
                 src="/posters/characters.jpg"
-                className="aspect-7/5 w-148 max-w-none rounded-2xl bg-gray-50 object-cover"
+                className="aspect-7/5 w-148 max-w-none rounded-2xl bg-gray-50 object-cover hover:opacity-75 
+          transition-opacity duration-300"
               />
             </div>
             <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-148 lg:items-start lg:justify-end lg:gap-x-8">
@@ -31,21 +34,24 @@ export default function About() {
                 <img
                   alt=""
                   src="/posters/default_character.jpg"
-                  className="aspect-4/3 w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                  className="aspect-4/3 w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover hover:opacity-75 
+          transition-opacity duration-300"
                 />
               </div>
               <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
                 <img
                   alt=""
                   src="/posters/tatooine.jpg"
-                  className="aspect-7/5 w-148 max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
+                  className="aspect-7/5 w-148 max-w-none flex-none rounded-2xl bg-gray-50 object-cover hover:opacity-75 
+          transition-opacity duration-300"
                 />
               </div>
               <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
                 <img
                   alt=""
                   src="/posters/darth_vader.jpg"
-                  className="aspect-4/3 w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
+                  className="aspect-4/3 w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover hover:opacity-75 
+          transition-opacity duration-300"
                 />
               </div>
             </div>
