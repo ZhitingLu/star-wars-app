df7819c56ea679ce4728079c1798a9d3270560f4
Zhiting Lu <zhitinglu@outlook.com>
2025-07-06 20:51:05 +0200

    style: improve formatting and indentation in page.js for better readability

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,8 +6,8 @@ import PlanetsTable from "@/components/PlanetsTable";
 
 export default function Home() {
   return (
-   <div className="min-h-screen relative text-white">
-    {/* Background video behind everything */}
+    <div className="min-h-screen relative text-white">
+      {/* Background video behind everything */}
       <video
         autoPlay
         loop
@@ -18,25 +18,33 @@ export default function Home() {
         <source src="/stars.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-    <HeroPage />
-    <main className="bg-black/70 text-white px-0 sm:px-6  py-8">
+      <HeroPage />
+      <main className="bg-black/70 text-white px-0 sm:px-6  py-8">
         <section id="people" className="my-10">
-          <h2 className="text-3xl font-bold mb-6 text-center
-            ">People</h2>
+          <h2
+            className="text-3xl font-bold mb-6 text-center
+            "
+          >
+            People
+          </h2>
           <PeopleTable />
         </section>
 
         <section id="planets" className="my-10">
-          <h2 className="text-3xl font-bold mb-6 text-center gradient-shadow-text">Planets</h2>
+          <h2 className="text-3xl font-bold mb-6 text-center gradient-shadow-text">
+            Planets
+          </h2>
           <PlanetsTable />
         </section>
 
-         <section id="about" className="my-10">
-          <h2 className="text-3xl font-bold mb-6 text-center gradient-shadow-text">About</h2>
-           <About />
+        <section id="about" className="my-10">
+          <h2 className="text-3xl font-bold mb-6 text-center gradient-shadow-text">
+            About
+          </h2>
+          <About />
         </section>
       </main>
-     <Footer />
-   </div>
+      <Footer />
+    </div>
   );
 }
